565ff034eacfbe62ba2c10f21f64c1a4769f60c8
Roger Chen <mengqiu.chen@hotmail.com>
2021-12-14 10:58:39 -0700

    Finish note functions

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/NoteController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/NoteController.java
@@ -1,31 +1,60 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
 import com.udacity.jwdnd.course1.cloudstorage.model.NoteForm;
 import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 
 public class NoteController {
-    private NoteService noteService;
+    private final NoteService noteService;
 
     public NoteController (NoteService noteService) {
         this.noteService = noteService;
     }
 
 
-    @PostMapping("/add")
+    @PostMapping("/note/add")
     public String addNote(NoteForm noteForm, Model model, Authentication authentication) {
 
         String title = noteForm.getTitle();
         String description = noteForm.getDescription();
+        String noteId = noteForm.getNoteId();
         String username = authentication.getName();
-        noteService.addNote(title, description, username);
+
+        try {
+            if (noteId == null || noteId.equals("")) {
+                noteService.addNote(title, description, username);
+            } else {
+                int id = Integer.parseInt(noteId);
+                Note note = noteService.getNoteById(id);
+                noteService.update(note.getId(), title, description);
+            }
+            model.addAttribute("result", "success");
+        } catch (Exception e) {
+            e.printStackTrace();
+            model.addAttribute("result", "fail");
+        }
+        model.addAttribute("notes", noteService.getAllNotes(authentication));
+        return "/result";
+    }
+
+    @GetMapping("/note/delete/{noteId}")
+    public String deleteNote (@PathVariable Integer noteId
+                                , @ModelAttribute NoteForm noteForm
+                                , Model model, Authentication authentication) {
+        try {
+            noteService.deleteNoteById(noteId);
+            model.addAttribute("result", "success");
+        } catch (Exception e) {
+            e.printStackTrace();
+            model.addAttribute("result", "fail");
+        }
+        model.addAttribute("notes", noteService.getAllNotes(authentication));
 
         return "/result";
     }
