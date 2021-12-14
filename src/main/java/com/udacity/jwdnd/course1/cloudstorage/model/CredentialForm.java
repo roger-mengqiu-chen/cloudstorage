81fc1a4a82565e8fc1c1976e25e3a3767d9c26bc
Roger Chen <mengqiu.chen@hotmail.com>
2021-12-14 12:40:01 -0700

    Create CredentialForm.java

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/CredentialForm.java
@@ -0,0 +1,34 @@
+package com.udacity.jwdnd.course1.cloudstorage.model;
+
+public class CredentialForm {
+    private String url;
+    private String username;
+    private String password;
+
+    public CredentialForm() {
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
