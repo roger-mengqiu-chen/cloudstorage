4019ba5a0d8898c98ffde03ae1a7aba8404d1819
Roger Chen <mengqiu.chen@hotmail.com>
2021-12-14 19:01:38 -0700

    Create FileMapper

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/FileMapper.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/FileMapper.java
@@ -1,4 +1,26 @@
 package com.udacity.jwdnd.course1.cloudstorage.mapper;
 
+import com.udacity.jwdnd.course1.cloudstorage.model.File;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
 public interface FileMapper {
+
+    @Insert("INSERT INTO FILES " +
+            "(filename, contenttype, filesize, userid, filedata) " +
+            "VALUES (#{fileName}, #{contentType}, #{fileSize}, #{userId}, #{fileData}})")
+    @Options(useGeneratedKeys = true, keyProperty = "fileId")
+    int addFile(File file);
+
+    @Delete("DELETE FROM FILES " +
+            "WHERE fileId = #{id}")
+    int deleteFile(int id);
+
+    @Select("SELECT * FROM FILES WHERE fileId = #{id}")
+    File getFile(int id);
+
+    @Select("SELECT * FROM FILES WHERE userid = #{userId}")
+    List<File> getAllFiles(int userId);
 }
