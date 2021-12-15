e10bf5c1cbdb8d4ddc4ad40be0809c8d773ab74f
Roger Chen <mengqiu.chen@hotmail.com>
2021-12-15 15:46:28 -0700

    Check if the success banner appear

--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/ResultPage.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/ResultPage.java
@@ -1,5 +1,6 @@
 package com.udacity.jwdnd.course1.cloudstorage;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -16,6 +17,10 @@ public class ResultPage {
     public ResultPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
         js = (JavascriptExecutor) driver;
+    }
+
+    public boolean isSuccess(WebDriver driver) {
+        return driver.findElement(By.id("aResultSuccess")) != null;
     }
 
     public void clickOK() {
